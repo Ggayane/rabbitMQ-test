a173a95e09a56a3d7d29bac28efe66e1b59d77d5
gayane <ggasparyan.gayane@gmail.com>
2018-01-11 17:53:07 +0400

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const amqp = require('amqplib/callback_api');
+const fs = require('fs');
+
+const img = './images/doy.png';
+
+const uvBuffer = fs.readFileSync(img, null).buffer;
+
+amqp.connect('amqp://localhost', (err, conn) => {
+    conn.createChannel((err, ch) => {
+        const q = 'render_queue';
+
+        ch.assertQueue(q, { durable: false });
+        // Note: on Node 6 Buffer.from(msg) should be used
+        ch.sendToQueue(q, new Buffer(uvBuffer), { replyTo: 'back', correlationId: '1' });
+        // correlationId ??????????
+        console.log(" [x] Sent %s", uvBuffer);
+    });
+
+    conn.createChannel((err, ch) => {
+        const q = 'back';
+
+        ch.assertQueue(q, { durable: false });
+        // Receiving
+        ch.consume(q, msg => {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, { noAck: true });
+
+    });
+});
